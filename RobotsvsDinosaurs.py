3a8798e316c4dba075b387d4826a85e566e36ade
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-19 14:56:12 -0600

    added dino classes with attacks, energy, health. created funstons to assign attacks.

--- RobotsvsDinosaurs.py
+++ RobotsvsDinosaurs.py
@@ -46,4 +46,57 @@ calculon = Robots("Calculon")
 
 bender = Robots("Bender")
 
+marvin = Robots("Marvin")
 
+six = Robots("Six")
+
+
+class Dinos:
+    def __init__(self):
+        self.name = ''
+        self.type = ''
+        self.health = 100
+        self.energy = 100
+        self.attack = ''
+
+
+def set_name(self):
+    self.name = input("enter dino name")
+    print("Dino name:", self.name)
+
+
+rodan = Dinos("Rodan")
+
+grumpy = Dinos("Grumpy")
+
+gorgo = Dinos("Gorgo")
+
+yoshi = Dinos("Yoshi")
+
+
+def power_dinos(self, amount, account):
+    if account == 1:
+        self.health.add(amount)
+    else:
+        self.power_level.add(amount)
+    return self
+
+
+def get_random_attack(min_value, max_value):
+    return random.randint(min_value, max_value)
+
+
+def get_attack_from_collection(options):
+    random_int = get_random_attack(0, len(options) - 1)
+    return options(random_int)
+
+
+def attack_assignment():
+    weapons = ('razor claws', 'spike tail', 'sonic blast', 'thunderous stomp')
+
+    satisfied = 'no'
+
+    while satisfied != 'yes':
+        chosen_attack = get_attack_from_collection(attack)
+
+        satisfied = input(f"Your attack is {chosen_attack}. Is this deadly enough?")
