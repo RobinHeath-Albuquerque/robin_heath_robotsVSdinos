7b0e0c0291cf89239ed39836cc7d11806efd576a
RobinHeath-Albuquerque <79651450+RobinHeath-Albuquerque@users.noreply.github.com>
2021-03-21 11:00:17 -0600

    Added lists for fleets and heards, added attack function for robots

---
+++ main.py
@@ -0,0 +1,17 @@
+from RobotsvsDinosaurs import Robots, Dinos
+
+if __name__ == '__main__':
+    robotOne = Robots()
+    robotOne.set_name()
+    robotOne.health(100)
+    robotOne.power_level(100)
+    robotOne.weapon()
+
+if __name__ == 'main__':
+    dinoOne = Dinos()
+    dinoOne.set_name()
+    dinoOne.type()
+    dinoOne.health(100)
+    dinoOne.energy(100)
+    dinoOne.attack()
+
